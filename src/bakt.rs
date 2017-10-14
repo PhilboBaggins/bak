ba3571a6cc25cf75055b5f58897eb82cfcee130c
Phil <PhilboBaggins@users.noreply.github.com>
2017-10-14 13:19:12 +1100

    Move common code to common code file

--- src/bakt.rs
+++ src/bakt.rs
@@ -1,39 +1,12 @@
-extern crate chrono;
-extern crate clap;
-extern crate fs_extra;
+extern crate bak_common;
+use bak_common::bak_bak_bak;
 
-use std::path::Path;
+extern crate chrono;
 use chrono::prelude::*;
-use clap::{App, Arg};
-//use fs_extra::file::copy_with_progress;
-use fs_extra::file::*;
 
 fn main() {
-    let matches = App::new("bakt")
-        .version("1.0")
-        .about("Does great things!")
-        .author("Phil B.")
-        .arg(Arg::with_name("file")
-            .help("File to backup")
-            .takes_value(true)
-            .required(true)
-            .multiple(true))
-        .get_matches();
-
-    for src_file in matches.values_of("file").unwrap() {
+    bak_bak_bak("bak", |src_file| {
         let dt: DateTime<Local> = Local::now();
-
-        let new_name = format!("{}_{}.bak", src_file, dt.format("%Y-%m-%d_%H-%M-%S"));
-
-        let path_from = Path::new(src_file);
-        let path_to = Path::new(&new_name);
-
-        let options = CopyOptions::new();
-        let handle = |process_info: TransitProcess|
-            print!("\r{:.2}%", process_info.copied_bytes as f64 / process_info.total_bytes as f64 * 100.0);
-        match fs_extra::file::copy_with_progress(path_from, path_to, &options, handle) {
-            Ok(_) => println!("\rCopied {} -> {}", path_from.to_str().unwrap(), path_to.to_str().unwrap()),
-            Err(x) => println!("\n{:?}", x),
-        }
-    }
+        format!("{}_{}.bak", src_file, dt.format("%Y-%m-%d_%H-%M-%S"))
+    });
 }
---
+++ src/common.rs
@@ -0,0 +1,35 @@
+extern crate clap;
+extern crate fs_extra;
+
+use std::path::Path;
+use clap::{App, Arg};
+//use fs_extra::file::copy_with_progress;
+use fs_extra::file::*;
+
+pub fn bak_bak_bak(name: &str, gen_name: fn(&str) -> std::string::String) {
+    let matches = App::new(name)
+        .version("1.0")
+        .about("Does great things!")
+        .author("Phil B.")
+        .arg(Arg::with_name("file")
+            .help("File to backup")
+            .takes_value(true)
+            .required(true)
+            .multiple(true))
+        .get_matches();
+
+    for src_file in matches.values_of("file").unwrap() {
+        let new_name = gen_name(src_file);
+
+        let path_from = Path::new(src_file);
+        let path_to = Path::new(&new_name);
+
+        let options = CopyOptions::new();
+        let handle = |process_info: TransitProcess|
+            print!("\r{:.2}%", process_info.copied_bytes as f64 / process_info.total_bytes as f64 * 100.0);
+        match fs_extra::file::copy_with_progress(path_from, path_to, &options, handle) {
+            Ok(_) => println!("\rCopied {} -> {}", path_from.to_str().unwrap(), path_to.to_str().unwrap()),
+            Err(x) => println!("\n{:?}", x),
+        }
+    }
+}
