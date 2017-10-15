bb495f45f0787334183417a8644f6f3e8c57d0e7
Phil <PhilboBaggins@users.noreply.github.com>
2017-10-15 15:06:02 +1100

    Implement bak program

--- src/bak.rs
+++ src/bak.rs
@@ -1,3 +1,8 @@
+extern crate bak_common;
+use bak_common::bak_bak_bak;
+
 fn main() {
-    println!("Placeholder for bak");
+    bak_bak_bak("bak", |src_file| {
+        format!("{}.bak", src_file)
+    });
 }
