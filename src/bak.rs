69530b45be635219beb232750621446c71bf69dc
Phil <PhilboBaggins@users.noreply.github.com>
2024-01-31 16:18:28 +1100

    cargo fmt

--- src/bak.rs
+++ src/bak.rs
@@ -3,8 +3,7 @@ use bak_common::bak_bak_bak;
 
 fn main() {
     let name = "bak";
-    let description = "Backup specified file to a new file with \".bak\" at the end of the new file name";
-    bak_bak_bak(name, description, |src_file| {
-        format!("{}.bak", src_file)
-    });
+    let description =
+        "Backup specified file to a new file with \".bak\" at the end of the new file name";
+    bak_bak_bak(name, description, |src_file| format!("{}.bak", src_file));
 }
--- src/common.rs
+++ src/common.rs
@@ -1,20 +1,26 @@
 extern crate clap;
 extern crate fs_extra;
 
+use clap::{crate_version, Arg, ArgAction, Command};
 use std::path::Path;
-use clap::{Command, Arg, ArgAction, crate_version};
 //use fs_extra::file::copy_with_progress;
 use fs_extra::file::*;
 
-pub fn bak_bak_bak(name: &'static str, description: &'static str, gen_name: fn(&str) -> std::string::String) {
+pub fn bak_bak_bak(
+    name: &'static str,
+    description: &'static str,
+    gen_name: fn(&str) -> std::string::String,
+) {
     let matches = Command::new(name)
         .version(crate_version!())
         .about(description)
         .author("Phil B.")
-        .arg(Arg::new("file")
-            .help("File to backup")
-            .action(ArgAction::Append)
-            .required(true))
+        .arg(
+            Arg::new("file")
+                .help("File to backup")
+                .action(ArgAction::Append)
+                .required(true),
+        )
         .get_matches();
 
     let files_to_backup = matches.get_many::<String>("file").unwrap();
@@ -25,10 +31,18 @@ pub fn bak_bak_bak(name: &'static str, description: &'static str, gen_name: fn(&
         let path_to = Path::new(&new_name);
 
         let options = CopyOptions::new();
-        let handle = |process_info: TransitProcess|
-            eprint!("\r{:.2}%", process_info.copied_bytes as f64 / process_info.total_bytes as f64 * 100.0);
+        let handle = |process_info: TransitProcess| {
+            eprint!(
+                "\r{:.2}%",
+                process_info.copied_bytes as f64 / process_info.total_bytes as f64 * 100.0
+            )
+        };
         match fs_extra::file::copy_with_progress(path_from, path_to, &options, handle) {
-            Ok(_) => eprintln!("\rCopied {} -> {}", path_from.to_str().unwrap(), path_to.to_str().unwrap()),
+            Ok(_) => eprintln!(
+                "\rCopied {} -> {}",
+                path_from.to_str().unwrap(),
+                path_to.to_str().unwrap()
+            ),
             Err(x) => eprintln!("\n{}", x),
         }
     }
